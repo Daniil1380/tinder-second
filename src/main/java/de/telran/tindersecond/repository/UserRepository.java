c872212859da60495651b7e6fe4b86a1c151a14a
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-12-18 22:01:18 +0300

    commit

--- src/main/java/de/telran/tindersecond/repository/UserRepository.java
+++ src/main/java/de/telran/tindersecond/repository/UserRepository.java
@@ -2,6 +2,7 @@ package de.telran.tindersecond.repository;
 
 import de.telran.tindersecond.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> getUsersByNameStartingWith(String name);
+    List<User> getUsersByNameStartingWithIgnoreCase(String name);
     //Select * from user where name like 'то, что вы вставите + %'
 
 }
