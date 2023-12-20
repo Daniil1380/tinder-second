4140bff6b5d3599bc7afa729d6627d31de1cca46
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-12-20 22:42:06 +0300

    commit

--- src/main/java/de/telran/tindersecond/MyFirstAspect.java
+++ src/main/java/de/telran/tindersecond/MyFirstAspect.java
@@ -1,10 +1,20 @@
 package de.telran.tindersecond;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class MyFirstAspect {
+
+    @Pointcut("execution(public void de.telran.tindersecond.service.impl.RatingServiceImpl.shareRating(Long, Long, Integer))")
+    public void method() {}
+
+    @Before("method()")
+    public void doSomething() {
+        System.out.println("Aspect is working, this is method shareRating");
+    }
 
 }
