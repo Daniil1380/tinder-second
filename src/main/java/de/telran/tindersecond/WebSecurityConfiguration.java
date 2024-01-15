dedfeed92d887840dc92e2c74e63fa5caecb55ba
d.tkachenko <d.tkachenko@argustelecom.ru>
2024-01-16 00:02:54 +0300

    commit

--- src/main/java/de/telran/tindersecond/WebSecurityConfiguration.java
+++ src/main/java/de/telran/tindersecond/WebSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -68,7 +69,10 @@ public class WebSecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(bCryptPasswordEncoder.encode("qwerty"));
+        System.out.println(bCryptPasswordEncoder.encode("Daniil"));
+        return bCryptPasswordEncoder;
     }
 
 
