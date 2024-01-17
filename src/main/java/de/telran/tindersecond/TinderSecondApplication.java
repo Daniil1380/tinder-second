a40c6b995f8f26b67e040f14ed4df76d0731cfde
d.tkachenko <d.tkachenko@argustelecom.ru>
2024-01-17 20:57:07 +0300

    commit

--- src/main/java/de/telran/tindersecond/TinderSecondApplication.java
+++ src/main/java/de/telran/tindersecond/TinderSecondApplication.java
@@ -1,9 +1,7 @@
 package de.telran.tindersecond;
 
-import de.telran.tindersecond.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
--- src/main/java/de/telran/tindersecond/controller/UserController.java
+++ src/main/java/de/telran/tindersecond/controller/UserController.java
@@ -1,25 +1,34 @@
 package de.telran.tindersecond.controller;
 
 import de.telran.tindersecond.ShareRatingDto;
+import de.telran.tindersecond.entity.SecurityAccount;
 import de.telran.tindersecond.entity.User;
 import de.telran.tindersecond.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Security;
 import java.util.List;
 
 @RestController //создается контроллер и управляется спрингом
 @RequiredArgsConstructor
 public class UserController {
 
+    @Value("${url.google}")
+    private String url;
+
     private final UserService userService;
 
     @GetMapping(value = "/show-new-user")
     public ResponseEntity<User> showNewUser() {
+        SecurityAccount securityAccount = (SecurityAccount) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userService.getNewUser();
 
         if (user != null) {
